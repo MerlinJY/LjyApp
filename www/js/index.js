076625be7a3cd2ee255f006731d5c9a8dd5e98c5
MerlinJY <MerlinJY@gmail.com>
2020-01-10 16:02:55 +0800

    onsen ui

--- www/js/index.js
+++ www/js/index.js
@@ -6,17 +6,7 @@ var app = {
         this.receivedEvent('deviceready');
     },
     receivedEvent: function(id) {
-        //var parentElement = document.getElementById(id);
-        //var listeningElement = parentElement.querySelector('.listening');
-        //var receivedElement = parentElement.querySelector('.received');
-
-        //listeningElement.setAttribute('style', 'display:none;');
-        //receivedElement.setAttribute('style', 'display:block;');
-
-        //console.log('Received Event: ' + id);
-
-        let db = window.openDatabase('ss', '1.0', 'ss', 2097152);
-        document.getElementById('text_Message').innerText = db.version;
+        let db = window.openDatabase('dbSalary', '1.0', 'Salary', 10485760);
     }
 };
 app.initialize();
