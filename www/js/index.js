15c1ed13f18d6f9899dc2129121d6ce022dd744a
MerlinJY <MerlinJY@gmail.com>
2020-01-10 10:52:45 +0800

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -6,14 +6,14 @@ var app = {
         this.receivedEvent('deviceready');
     },
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        //var parentElement = document.getElementById(id);
+        //var listeningElement = parentElement.querySelector('.listening');
+        //var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        //listeningElement.setAttribute('style', 'display:none;');
+        //receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+        //console.log('Received Event: ' + id);
 
         let db = window.openDatabase('ss', '1.0', 'ss', 2097152);
         document.getElementById('text_Message').innerText = db.version;
